5f506bafd82a796415e479da10e30fc129b1010a
FilipS <filips@vaskir.co>
2017-05-22 13:52:36 +0200

    updated build.sh
    `make` to `sudo make`

--- cpuminer-source/build.sh
+++ cpuminer-source/build.sh
@@ -3,4 +3,4 @@ sudo apt-get update -y
 sudo apt-get install libusb-1.0-0-dev libusb-1.0-0 libcurl4-openssl-dev libncurses5-dev libudev-dev make automake autoconf -y
 sudo ./autogen.sh
 sudo ./configure CFLAGS="-O3"
-make
+sudo make
